e7153adaa70f64cfe04acbf51cbf918716d5366a
Beatriz Santana <afrobia.dev@gmail.com>
2025-06-19 11:50:30 -0300

    implementando o model de product e teste unitario da classe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { CouponsModule } from './coupons/coupons.module';
+import { DiscountManagementModule } from './discount-management/discount-management.module';
+
 
 @Module({
-  imports: [],
+  imports: [ProductsModule /*, CouponsModule, DiscountManagementModule */],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/products/model/product.ts
@@ -0,0 +1,36 @@
+import { ProductModel } from "./product-interface";
+
+
+export class Product implements ProductModel {
+    private id: number;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date | null
+
+    constructor(
+        name: string,
+        description: string,
+        price: number,
+        stock: number,
+    ) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.deletedAt = null;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    setId(id: number): void {
+        this.id = id;
+    }
+}
