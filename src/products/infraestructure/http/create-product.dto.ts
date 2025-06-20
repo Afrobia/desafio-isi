db44358e85a7821ca51ad017da3296afcf8bcca0
Beatriz Santana <afrobia.dev@gmail.com>
2025-06-20 17:29:44 -0300

    validando con decorator no dto

--- src/products/infraestructure/http/create-product.dto.ts
+++ src/products/infraestructure/http/create-product.dto.ts
@@ -1,18 +1,20 @@
-import { IsNumber, IsString, MaxLength, MinLength } from "class-validator";
+import { IsDecimal, IsNotEmpty, IsNumber, IsString, Length, MaxLength, MinLength } from "class-validator";
 
 export class CreateProductDto {
   @IsString()
-  @MinLength(3)
-  @MaxLength(100)
+  @Length(3, 100,{ message: 'Name must be between 3 and 50 characters' })
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
 
   @IsString()
   @MaxLength(200)
   description: string;
 
-  @IsNumber()
+  @IsNumber({maxDecimalPlaces: 2}, { message: 'Price must be a number with up to two decimal places' })
+  @IsNotEmpty({ message: 'Price is required' })
   price: number;
 
   @IsNumber({},{ message: 'Stock must be a number' })
+
   stock: number;
 }
